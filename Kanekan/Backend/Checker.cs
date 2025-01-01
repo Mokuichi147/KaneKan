d31bfa1482bc0f07e3f6cb66fbca8a0f2174f36f
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-01-02 02:49:49 +0900

    Add PaymentDialog

---
+++ Kanekan/Backend/Checker.cs
@@ -0,0 +1,21 @@
+namespace Kanekan.Backend;
+
+public static class Checker
+{
+    public static bool IsPassedPayment(Models.Payment payment)
+    {
+        bool result = true;
+        result &= IsPassedPaymentName(payment.Name);
+        return result;
+    }
+
+    public static bool IsPassedPaymentName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
