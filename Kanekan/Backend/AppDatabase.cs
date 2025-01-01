5b886c8be8138a75cc6ed6d5b937add809a3bbe5
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-01-02 02:49:26 +0900

    Update Payment

--- Kanekan/Backend/AppDatabase.cs
+++ Kanekan/Backend/AppDatabase.cs
@@ -7,7 +7,7 @@ public static class AppDatabase
     public static List<Payment> GetPayments()
     {
         using var context = new AppDbContext(isReadOnly: true);
-        return [.. context.Payments.OrderBy(o => o.CreatedAt)];
+        return [.. context.Payments.OrderByDescending(x => x.PaymentAt ?? x.CreatedAt)];
     }
 
     public static List<Payment> GetPayments(DateTime startDate, DateTime endDate, bool isContainNullPaymentAt)
@@ -29,11 +29,27 @@ public static class AppDatabase
     public static async Task SavePayment(Payment payment)
     {
         using var context = new AppDbContext(isReadOnly: false);
+        Payment? _payment = await context.Payments.FindAsync(payment.Id);
         using (var transaction = await context.Database.BeginTransactionAsync())
         {
             try
             {
-                context.Payments.Add(payment);
+                if (_payment != null)
+                {
+                    _payment.CreatedAt = payment.CreatedAt;
+                    _payment.PaymentAt = payment.PaymentAt;
+                    _payment.IsPaid = payment.IsPaid;
+                    _payment.Name = payment.Name;
+                    _payment.Amount = payment.Amount;
+                    _payment.PaymentMethod = payment.PaymentMethod;
+                    _payment.Description = payment.Description;
+                    _payment.CategoriesJson = payment.CategoriesJson;
+                }
+                else
+                {
+                    context.Payments.Add(payment);
+                }
+                
                 await context.SaveChangesAsync();
                 await transaction.CommitAsync();
             }
@@ -43,5 +59,34 @@ public static class AppDatabase
                 throw;
             }
         }
+    }
+
+    public static List<string> GetPaymentMethods(int loadCount = 100)
+    {
+        using var context = new AppDbContext(isReadOnly: true);
+        return context.Payments.OrderByDescending(x => x.CreatedAt)
+                               .Take(loadCount)
+                               .AsEnumerable()
+                               .Where(x => x.PaymentMethod != null)
+                               .Select(x => x.PaymentMethod ?? throw new Exception("PaymentMethod is null"))
+                               .GroupBy(x => x)
+                               .ToDictionary(x => x.Key, x => x.Count())
+                               .OrderByDescending(x => x.Value)
+                               .Select(x => x.Key)
+                               .ToList();
+    }
+
+    public static List<string> GetCategories(int loadCount = 100)
+    {
+        using var context = new AppDbContext(isReadOnly: true);
+        return context.Payments.OrderByDescending(x => x.CreatedAt)
+                               .Take(loadCount)
+                               .AsEnumerable()
+                               .SelectMany(x => x.Categories)
+                               .GroupBy(x => x)
+                               .ToDictionary(x => x.Key, x => x.Count())
+                               .OrderByDescending(x => x.Value)
+                               .Select(x => x.Key)
+                               .ToList();
     }
 }
